b6cdd11b1e50080350f6189b6b21c104d4af594e
Sudhir1718 <119098804+Sudhir1718@users.noreply.github.com>
2023-02-21 22:03:51 +0530

    Update a1.java

--- sudhir/src/main/java/com/example/demo/a1.java
+++ sudhir/src/main/java/com/example/demo/a1.java
@@ -6,19 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class a1 {
-	@GetMapping("/welcome")
-	public String Welcome()
-	{
-		return "Welcome ";
-	}
+	
 	@GetMapping("/{name}")
 	public String hello(@PathVariable String name)
 	{
 		return "Welcome "+name+"!";
-	}
-	@GetMapping("/{col}")
-	public String MyFavCol(@PathVariable String col)
-	{
-		return "My  Favorite color is "+col;
 	}
 }
